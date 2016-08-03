7ee8ec95aaeeeade330731658f66f1b0ddf1ec58
rigoneri <rodrigo@rigoneri.com>
2016-08-03 08:57:26 -0500

    Added a maxZoom level to the Foursquare map so it doesn't look bad when there's only one data point

--- client/app/scripts/foursquare.js
+++ client/app/scripts/foursquare.js
@@ -37,7 +37,7 @@ angular.module('clientApp')
           running = false;
         });
       }
-      
+
       angular.element($window).bind('scroll', handleScroll);
       $scope.$on('$destroy', function() {
         angular.element($window).unbind('scroll', handleScroll);
@@ -157,11 +157,12 @@ angular.module('clientApp')
         $window.GoogleMapsLoader.KEY = GOOGLE_MAPS_KEY;
         $window.GoogleMapsLoader.load(function(google) {
           map = new google.maps.Map(document.getElementById('foursquare-map'), {
-            center: { 
+            center: {
               lat: groupedByLocation[0].lat,
               lng: groupedByLocation[0].lng
-            }, 
+            },
             zoom: 11,
+            maxZoom: 12,
             scrollwheel: false,
             options: {
               styles: [{'featureType':'all','elementType':'labels.text.fill','stylers':[{'color':'#000000'},{'lightness':'40'}]},{'featureType':'all','elementType':'labels.text.stroke','stylers':[{'visibility':'simplified'},{'color':'#000000'},{'lightness':16}]},{'featureType':'all','elementType':'labels.icon','stylers':[{'visibility':'off'}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'color':'#273239'},{'lightness':'52'},{'visibility':'off'}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'weight':'1.00'},{'visibility':'on'},{'lightness':'-61'}]},{'featureType':'administrative.province','elementType':'labels.text','stylers':[{'visibility':'simplified'}]},{'featureType':'landscape','elementType':'geometry','stylers':[{'color':'#273239'},{'lightness':'-18'},{'saturation':'0'}]},{'featureType':'landscape.natural','elementType':'geometry.fill','stylers':[{'visibility':'on'}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#273239'},{'lightness':'-9'},{'visibility':'on'}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#111619'},{'lightness':'5'}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'weight':0.2},{'color':'#273239'},{'lightness':'-50'},{'visibility':'off'}]},{'featureType':'road.arterial','elementType':'geometry','stylers':[{'color':'#111619'},{'lightness':'6'}]},{'featureType':'road.arterial','elementType':'labels.text','stylers':[{'visibility':'off'}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#111619'},{'lightness':'3'}]},{'featureType':'road.local','elementType':'labels.text','stylers':[{'visibility':'off'}]},{'featureType':'transit','elementType':'geometry','stylers':[{'color':'#273239'},{'lightness':'-9'}]},{'featureType':'water','elementType':'geometry','stylers':[{'color':'#182125'},{'lightness':'-12'},{'saturation':'0'},{'gamma':'1.00'}]},{'featureType':'water','elementType':'labels.text','stylers':[{'visibility':'simplified'}]}],
@@ -236,17 +237,17 @@ angular.module('clientApp')
               $scope.animateEnter = false;
               if (!$scope.$$phase) {
                 $scope.$apply();
-              } 
+              }
             }, 1200);
           }, time);
         }
       }
 
-      _getUser(function() { 
+      _getUser(function() {
         var start = new Date().getTime();
-        var didLoadAnimateFirst = false; 
+        var didLoadAnimateFirst = false;
 
-        var loadTimeout = setTimeout(function() { 
+        var loadTimeout = setTimeout(function() {
           if (!didLoadAnimateFirst) {
             didLoadAnimateFirst = true;
             _animateEnter(0);
@@ -287,7 +288,7 @@ angular.module('clientApp')
           } else {
             fetching = false;
             cb(null);
-          } 
+          }
         }).error(function(data) {
           console.log('Error', data);
           fetching = false;
